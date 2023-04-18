2c8e57978ec50064bcbb43eb136f3e4261249638
adrianboh@gmail.com <adrianboh@gmail.com>
2023-04-18 15:01:28 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-crud-webapi/Models/PizzaFormModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaFormModel
+    {
+        public Pizza Pizza { get; set; }
+
+        public PizzaFormModel(Pizza pizza)
+        {
+            Pizza = pizza;
+        }
+
+        public PizzaFormModel()
+        {
+        }
+
+        public List<Categoria>? Categories { get; set; }
+        public List<SelectListItem>? Ingredients { get; set; }
+        public List<string>? SelectedIngredients { get; set; }
+    }
+}
